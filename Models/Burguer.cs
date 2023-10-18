fc4055fd17686fa1b66d42c399360eab2b387efe
javier.varela <javier.varela@udla.edu.ec>
2023-10-17 21:00:41 -0500

    Creacion del modelo Burguer

---
+++ Models/Burguer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudPractica.Models
+{
+    public class Burguer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        [Range(0.01, 99.99)]
+        public decimal Price  { get; set; }
+    }
+}
