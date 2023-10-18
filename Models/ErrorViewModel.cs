57b5b4a9cd8cae13461d69c3c6cd5aa7f62edd77
javier.varela <javier.varela@mindsoft.biz>
2023-10-17 20:41:54 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudPractica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
